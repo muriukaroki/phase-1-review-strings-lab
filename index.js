87fe793325d6e683d3560779fafb69b2395ca750
Justin <justin.kjaroki@student.moringaschool.com>
2022-08-21 00:59:52 +0300

    Finished lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = "Jimmy Mutuku";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+
+const shortGreeting  = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+console.log(shortGreeting);
